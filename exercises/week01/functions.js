7ef78ab89e2c23ec758ffe3a7a13e633e789f1ee
aetherdies <N00230487@iadt.ie>
2024-03-04 12:52:21 +0000

    just starting off

---
+++ exercises/week01/functions.js
@@ -0,0 +1,32 @@
+function square(x) {
+    let result= x * x;
+    return result;
+}
+
+let a = 10;
+let b = square(a);
+console.log("The square of " + a + "is " + b);
+
+const square = function (maths) {
+    const result = x * y;
+    return maths * result;
+};
+
+let y = 1000; 
+
+console.log(square);
+
+let isGreaterThan = (num1, num2) => {
+    if(num1 > num2) {
+        return true;
+    }
+    else {
+        return false;
+    }
+};
+
+console.log(isGreaterThan(10, 5));
+
+setTimeout(() => {
+    console.log("3 seconds have elapsed!")
+}, 3000);
